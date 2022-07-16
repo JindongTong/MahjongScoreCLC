3dcd73f335bafcd9a80330d807a28ad725bb8475
JindongTOng <1120594024@qq.com>
2022-07-16 21:36:29 +0800

    cal

---
+++ cal.js
@@ -0,0 +1,34 @@
+function test(a) {
+    console.log(a);
+}
+
+hand = []
+
+// fghjk
+/*
+*/
+// 第一步 拿到hand之后 按照役种拆分
+// 从三番开始拆
+
+// hand 先过is一气通贯 拆剩下的拍去过ishu
+function isyiqi
+
+{ishu(())}
+function ishu(hand) {//手牌 + 拆分 + 副露
+
+//3*n+2 如果
+    
+}
+function ishuqiduizi(hand)
+[ [1,2,3],[2,3,4],[-5,-6,-7],[7,8,9],[4,4]]
+
+// hand: array, res: array[array] [[1,2,3],[4,4,4,4]] [{1,"shunzi"}, {4,"minggang"}]
+function chai(hand, result) {  //从左到右遍历定将
+    for (let i = 0; i < hand.length; i++) {
+        //定将
+    }
+    function chaimianzi(hand,result){
+        //递归出所有可能结果
+    }
+    //加上将 chai返回结果
+}
